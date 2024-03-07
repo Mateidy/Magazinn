b41d9558c3219577c5c17ede28428e646cc6d62b
Mateidy <148993027+Mateidy@users.noreply.github.com>
2024-03-07 11:32:42 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+import uuid
+from flask import Flask , request
+from flask_smorest import abort
+from db import items , stores
+app=Flask (__name__)
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores":list(stores.values())}
+
+@app.post("/store")
+def create_store():
+    store_data=request.get_json()
+    store_id=uuid.uuid4().hex
+    store={**store_data,"id":store_id}
+    stores[store_id]=new_store
+    return store, 201
+
+@app.post ("/item")
+def create_item(name):
+    item_data=request.get_json()
+    if item_data["store_id"] not in stores:
+        abort (404, message="Store not found")
+
+    item_id=uuid.uuid4().hex
+    item={**item_data,"id":item_id}
+    items[item_id]=item
+
+    return item,201
+
+@app.get("/item")
+def get_all_items():
+    return {"items":list(items.values())}
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort (404, message="Store not found")
+
+
+@app.get("/item<string:item>/item_id")
+def get_item(name):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort (404, message="item not found")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
---
+++ main.py
@@ -0,0 +1,2 @@
+from Flask import flask
+app=Flask (__name__)
