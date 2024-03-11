396ebfcc3a75a36c424968b35be4b2c81fe4c4ca
Mateidy <148993027+Mateidy@users.noreply.github.com>
2024-03-11 20:52:06 +0200

    Commit

--- app.py
+++ app.py
@@ -12,14 +12,36 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data=request.get_json()
+    if "name" not in store_data:
+        abort (
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON playload. " ,
+        )
+        for store in store.values():
+            if store_data["name"]==store["name"]:
+                abort (400, message=f"Store already exists .")
+
+
     store_id=uuid.uuid4().hex
     store={**store_data,"id":store_id}
-    stores[store_id]=new_store
+    stores[store_id]=store
     return store, 201
 
 @app.post ("/item")
-def create_item(name):
+def create_item():
     item_data=request.get_json()
+    if ("price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(400,
+              message="Bad request. Ensure 'price' ,'store_id, and 'name' are included in the JSON playload" ,)
+    for item in item.values():
+        if(
+            item_data["name"]==item["name"]
+            and item_data["store_id"]==item["store_id"]
+        ):
+            abort (400 , message=f"Item already exists.")
     if item_data["store_id"] not in stores:
         abort (404, message="Store not found")
 
@@ -29,9 +51,6 @@ def create_item(name):
 
     return item,201
 
-@app.get("/item")
-def get_all_items():
-    return {"items":list(items.values())}
 
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
@@ -48,6 +67,18 @@ def get_item(name):
     except KeyError:
         abort (404, message="item not found")
 
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message":" Item deleted."}
+    except KeyError:
+        abort (404,message="Item not found .")
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items":list(items.values())}
 
 if __name__ == "__main__":
     app.run(debug=True)
