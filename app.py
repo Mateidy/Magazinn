138adc4ee23d8687c49b30b7d6f92d6c277da733
Mateidy <148993027+Mateidy@users.noreply.github.com>
2024-05-13 18:20:50 +0300

    added by Mateidy

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
-from flask import Flask
+import secrets
+
+from flask import Flask , jsonify
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+
 
 
 from db import db
@@ -9,6 +13,8 @@ import models
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+
 
 
 def create_app(db_url=None):
@@ -28,11 +34,42 @@ def create_app(db_url=None):
 
     api=Api(app)
 
+    app.config["JWT_SECRET_KEY"]="295040099478539289169991040315992286316"
+    jwt=JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_loader(jwt_header,jwt_payload):
+        return (
+            jsonify({"message": "The token has expired." , "error": "token_expired"}),401,
+        )
+    @jwt.invalid_token_loader()
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message":"Signature verification failed." , "error":"invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {"description":"Request does not contain an acces token .",
+                 "error": "authorization_required" ,}
+            ),
+            401,
+        )
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
 
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
 
